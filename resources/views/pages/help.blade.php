2dc56f5d2ae6b8a502b8d7b65a9e587bb1ea8833
TNedim <ntuyyel@gmail.com>
2021-03-05 19:56:30 +0300

    help page edit

--- resources/views/pages/help.blade.php
+++ resources/views/pages/help.blade.php
@@ -7,7 +7,9 @@
     <div class="row">
       <div class="col-12 pt-4">
         <h1>Help and Support</h1>
-        <p>____</p>
+        <h4>Please call for help and support.</h4><br>
+        	<a href="tel:07771691736">0777 169 17 36</a><br>
+          <a href="tel:01243822822">01243 822 822</a><br>
       </div>
     </div>
   </div>
