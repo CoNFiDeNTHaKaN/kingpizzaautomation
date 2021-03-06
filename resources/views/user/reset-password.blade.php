945f40c63b59faf9fd36c950be51c68ea7553982
TNedim <ntuyyel@gmail.com>
2021-03-07 00:05:32 +0300

    reset password page

--- resources/views/user/reset-password.blade.php
+++ resources/views/user/reset-password.blade.php
@@ -1,27 +1,29 @@
-@extends('templates.slim')
+@extends('layouts.main')
 
-@push('contentClasses', 'kebab-bg')
 
 @section('pageTitle', 'Reset your password')
 
 @section('content')
-  <div class="wrapper flex flex--align-center">
+  <div class="container">
+  <div class="row mt-5">
+  <div class="col-12 mt-5">
 
-    <div class="modal modal--narrow t-center">
       <h2>Reset your password</h2>
+      {{$message ?? ''}}
+      <p>Please enter your email to reset your password.</p>
       <form method="post" enctype="multipart/form-data" action="{{ route('password.email') }}">
         {{ csrf_field() }}
-        <div class="formitem">
-          <label>Email</label>
-          <input type="email" name="email">
+        <div class="form-group">
+          <input class="form-control" type="email" name="email" placeholder="E-mail">
         </div>
         <div class="formitem flex flex--align-right">
           <input type="hidden" name="url" value="{{ request()->get('redirect_to') }}">
-          <input type="submit" value="Reset" class="button button--green">
+          <input type="submit" value="Reset" class="btn btn_1">
         </div>
 
       </form>
-    </div>
+      </div>
 
+    </div>
   </div>
 @endsection
